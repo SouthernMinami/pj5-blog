89859b192a3f64c07d65e2c8737d6b05b2425d4d
SouthernMinami <kanou373@gmail.com>
2024-03-29 23:54:44 +0900

    Add: Open Libraryから検索結果を取得してテーブルに保存

--- Commands/Programs/BookSearch.php
+++ Commands/Programs/BookSearch.php
@@ -9,14 +9,12 @@ use Database\MySQLWrapper;
 class BookSearch extends AbstractCommand
 {
     protected static ?string $alias = 'booksearch';
-    protected static array $cache = [];
-    protected MySQLWrapper $result = new MySQLWrapper();
 
     public static function getArgs(): array
     {
         return [
-            (new Argument('title'))->description('本のタイトルを入力してください。')->required(true)->allowAsShort(true),
-            (new Argument('isbn'))->description('ISBNを入力してください。')->required(true)->allowAsShort(true),
+            (new Argument('title'))->description('本のタイトルを入力してください。')->required(false)->allowAsShort(true),
+            (new Argument('isbn'))->description('ISBNを入力してください。')->required(false)->allowAsShort(true),
         ];
     }
 
@@ -26,22 +24,75 @@ class BookSearch extends AbstractCommand
         $isbn = $this->getArgValue('isbn');
 
         if ($title) {
-            $this->searchByTitle($title);
+            $title_input = readline("書籍のタイトルを入力してください: ");
+            $this->searchByTitle($title_input);
         } else if ($isbn) {
-            $this->searchByISBN($isbn);
+            $isbn_input = readline("ISBNを入力してください: ");
+            $this->searchByISBN($isbn_input);
         }
         return 0;
     }
 
     public function searchByTitle(string $title): void
     {
-        $cached_book = mysqli_fetch_assoc($this->result->query("SELECT * FROM books WHERE title = $title"));
-        if ($cached_book) {
-            $this->log(sprintf("この書籍のデータはすでにデータベースに存在します。%s", json_encode($cached_book)));
-        } else {
-            // book-search-title-???をキーとして、Open Libraryから取得したデータをキャッシュする
+        $mysqli = new MySQLWrapper();
+        $query = "CREATE TABLE IF NOT EXISTS books (
+            key_string varchar(255) PRIMARY KEY,
+            title varchar(255) NOT NULL,
+            author varchar(50) NOT NULL,
+            publisher varchar(50) NOT NULL,
+            isbn varchar(255) NOT NULL,
+            pages int,
+            created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+            updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+        )";
+        $mysqli->query($query);
 
+        $select_query = "SELECT * FROM books WHERE title = '$title'";
+        $result = $mysqli->query($select_query);
+
+        if ($result->num_rows === 0) {
+            // Open Libraryから取得したデータをDBに保存
+            $url = "https://openlibrary.org/search.json?title=$title";
+            $res = file_get_contents($url);
+            $data = json_decode($res, true);
+
+            $book_info = $data['docs'][0];
+            $title = $mysqli->real_escape_string($book_info['title']);
+            $key_string = "book-search-title-" . $title;
+            $author = $mysqli->real_escape_string($book_info['author_name'][0]);
+            $publisher = $mysqli->real_escape_string($book_info['publisher'][0]);
+            $isbn = $mysqli->real_escape_string($book_info['isbn'][0]);
+            $pages = intval($book_info['number_of_pages_median'][0]);
+
+            $insert_query = "INSERT INTO books (key_string, title, author, publisher, isbn, pages) VALUES (
+                '{$key_string}',
+                '{$title}',
+                '{$author}',
+                '{$publisher}',
+                '{$isbn}',
+                '{$pages}'
+            )";
+
+
+            $mysqli->query($insert_query);
+
+            $this->log("書籍情報を取得しました。");
+            $this->log("タイトル: " . $book_info['title']);
+
+            $this->log("書籍情報をデータベースに保存しました。");
+        } else {
+            $this->log("この書籍はデータベースに保存済みです。");
+            // while ($row = $result->fetch_assoc()) {
+            //     $this->log("タイトル: " . $row['title']);
+            //     $this->log("著者: " . $row['author']);
+            //     $this->log("出版社: " . $row['publisher']);
+            //     $this->log("ISBN: " . $row['isbn']);
+            //     $this->log("ページ数: " . $row['pages']);
+            //     $this->log("価格: " . $row['price']);
+            // }
         }
+
     }
 
     public function searchByISBN(string $isbn): void
